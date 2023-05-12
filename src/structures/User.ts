be602358a01bfa60d30e959d1a0a9498e4088756
Maytham <44939173+Maytha8@users.noreply.github.com>
2023-05-12 17:49:29 +0800

    remove __type property when converting to object

--- src/structures/User.ts
+++ src/structures/User.ts
@@ -66,7 +66,6 @@ export class User {
 
   toJSON(): UserDetailsBlob {
     return {
-      __type: 'UserDetailsBlob',
       chroniclePinnedCount: this.chroniclePinnedCount,
       userACTStudentID: this.ACTStudentID,
       userAccessRestrictions: this.accessRestrictions,
